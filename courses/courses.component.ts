1ec9d125e07c5a5c18535c2630e33c9323324f00
ilaan <benjailaan@gmail.com>
2022-07-02 10:19:09 +0300

    using class binding

--- courses/courses.component.ts
+++ courses/courses.component.ts
@@ -6,16 +6,26 @@ import { Component, OnInit } from '@angular/core';
   <h2>
     First trial below is:
   </h2>
-  <input [id]="myId" type="text" value="Vishwas">
-  <input [disabled]="true" id="{{myId}}" type="text" value="Vishwas">
+  
+  <h2 class="text-special">Coloring is fun</h2>
+  <h2 [class.text-danger]="hasError">This is the green text</h2>
   {{trial}}
   `,
-  styleUrls: ['./courses.component.css']
+  styles: [`
+  .text-special{
+    color:red;
+  }
+  .text-danger{
+    color:green;
+  }
+  
+  `]
 })
 export class CoursesComponent implements OnInit {
   public trial = "";
   public myId = "testId";
-
+  public hasError = true;
+  
   constructor() {
     
 
