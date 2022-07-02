9fb016e76ceb9477d2cd7c3397f22c23085ec6fc
ilaan <benjailaan@gmail.com>
2022-07-02 14:50:38 +0300

    sending data from child component to parent component

--- courses/courses.component.ts
+++ courses/courses.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, OnInit, NgModule, Input } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
   template: `
-  <h2>
-    First trial below is:
-  </h2>
-  
-  <div *ngFor="let color of colors;even as i">
-    <h2>{{i}} {{color}}</h2>
-  </div>
-  <!-- <div [ngSwitch]="color">
-    <div *ngSwitchCase="'red'">You picked the red color</div>
-    <div *ngSwitchCase="'blue'">You picked the blue color</div>
-    <div *ngSwitchCase="'green'">You picked the green color</div>
+    <h2>{{"Hello "+parentData}}</h2>
 
-  </div> -->
-
-  {{name}}
-  
-  {{greeting}}
-  {{trial}}
   `,
   styles: [`
   .text-special{
@@ -33,14 +17,8 @@ import { Component, OnInit, NgModule } from '@angular/core';
   `]
 })
 export class CoursesComponent implements OnInit {
-  public colors = ["red","blue","green"];
-  public color = "red";
-  displayName = true;
-  public name="";
-  public trial = "";
-  public myId = "testId";
-  public highlightColor = 'yellow';
-  public greeting ="";
+  
+  @Input() public parentData;
 
   constructor() {
     
@@ -50,17 +28,13 @@ export class CoursesComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  getStart(){
-    this.trial = "Impressive";
-
-  }
   logMessage(value:string){
     console.log(value);
   }
 
   onClick(event:Event){
     console.log(event);
-    this.greeting = 'Welcome to Coding of the evolution';
+
 
   }
 
