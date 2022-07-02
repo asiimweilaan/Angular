683664fc0fc13920186327b3aa5133bd6a6cf287
ilaan <benjailaan@gmail.com>
2022-07-02 15:35:17 +0300

    pipes in angular

--- courses/courses.component.ts
+++ courses/courses.component.ts
@@ -3,8 +3,25 @@ import { Component, OnInit, NgModule, Input, Output, EventEmitter } from '@angul
 @Component({
   selector: 'app-courses',
   template: `
-    <h2>{{"Hello "+parentData}}</h2>
-    <button (click)="fireEvent()">Send Event</button>
+    <!-- <h2>{{name | lowercase}}</h2>
+    <h2>{{name | uppercase}}</h2>
+    <h2>{{message | titlecase}}</h2> -->
+    <h2>{{name | slice:3:5}}</h2>
+    <h2>{{person | json}}</h2>
+
+    <h2>{{5.678 | number:'1.2-3'}}</h2>
+    <h2>{{5.678 | number:'3.4-5'}}</h2>
+    <h2>{{5.678 | number:'3.1-2'}}</h2>
+
+    <h2>{{ 0.25 | percent }}</h2>
+    <h2>{{ 0.25 | currency }}</h2>
+    <h2>{{ 0.25 | currency: 'GBP'}}</h2>
+
+    <h2>{{date}}</h2>
+    <h2>{{date | date:'short'}}</h2>
+    <h2>{{date | date:'shortDate'}}</h2>
+    <h2>{{date | date:'shortTime'}}</h2>
+
   `,
   styles: [`
   .text-special{
@@ -17,16 +34,17 @@ import { Component, OnInit, NgModule, Input, Output, EventEmitter } from '@angul
   `]
 })
 export class CoursesComponent implements OnInit {
-  
-  @Input() public parentData;
-  @Output() public childEvent = new EventEmitter();
+  public name = "Codevolution";
+  public message = "Welcome to Codevolution";
+  public person ={
+    "firstName":"John",
+    "lastName":"Doe"
+  }
+  public date = new Date();
 
   constructor() {
     
 
-  }
-  fireEvent(){
-    this.childEvent.emit('Hey Codevolution');
   }
 
   ngOnInit(): void {
